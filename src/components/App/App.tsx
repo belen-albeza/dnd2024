dd04dd15ba721b91d6cbcff06fd0e4df996e137b
Belén Albeza <belen@hey.com>
2025-04-05 09:18:27 +0200

    Set up project with Vite

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
